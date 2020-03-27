5170377370d19201755e46c42c6c8e462a4b4240
perringaux <perringaux@eisti.eu>
2020-03-28 00:55:29 +0100

    I added the function: write a post

--- COVDPX/models/db/db_profil.py
+++ COVDPX/models/db/db_profil.py
@@ -1,8 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class Profil(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,default=False)
     friends = models.ManyToManyField(User, related_name="friends")
 
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    text = models.TextField(null=True)
+    date = models.DateTimeField(default=timezone.now)
--- COVDPX/views/profil.py
+++ COVDPX/views/profil.py
@@ -1,24 +1,42 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from COVDPX.models.db.db_profil import Profil
+from COVDPX.models.db.db_profil import Profil, Post
+from COVDPX.models.forms.form_profil import PostForm
 from django.http import HttpResponse
 
 
-# user = User.objects.create_user('526', 'mkff5l@crepfes-bretonnes.com', 'm0nsup3rm0td3p4ss3')
-# user1 = User.objects.create_user('935', 'm1sskfl@crepes-bretonnes.com', 'm0nsup3rm0td3p4ss3')
-# profil1 = Profil.objects.create(user=user)
-# profil1.friends.add(user1)
-
-
 def profil(request, userId):
+
+    # page user
     user = User.objects.get(id=userId)
+
+    # list users
     users = User.objects.all()
+
+    # list friends
     profil = Profil.objects.get(user_id=userId)
     friends = profil.friends.all()
-    return render(request, "profil/profil_index.html", {"user": user, "users": users, "friends": friends})
+
+    # list posts
+    posts = Post.objects.filter(author_id=userId)
+
+    # post
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data.get('title')
+            text = form.cleaned_data.get('text')
+            author = request.user
+            post = Post.objects.create(tile=title, text=text, author=author)
+            post.save()
+    else:
+        form = PostForm()
+
+    return render(request, "profil/profil_index.html", {"user": user, "users": users, "friends": friends, "form": form, "posts": posts})
 
 
 def invitation(request, userId):
+
     profil = Profil.objects.get(user_id=request.user.id)
     friend = User.objects.get(id=userId)
     profil.friends.add(friend)
